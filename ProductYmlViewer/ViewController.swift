3d2b75c3d6270b4a7e01b2e001c86539bcfe8612
sergei <sb@slam.by>
2021-08-01 20:29:37 +0300

    Initial Commit

---
+++ ProductYmlViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductYmlViewer
+//
+//  Created by Сергей Бушкевич on 1.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
